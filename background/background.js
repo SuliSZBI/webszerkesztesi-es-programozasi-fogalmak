ef0921c411c1d5d9c8de7ec00be22f95b6b64d97
pepe1973 <bodrogipr@gmail.com>
2024-12-06 06:16:50 +0100

    2024.12.06. Mentés

---
+++ background/background.js
@@ -0,0 +1,12 @@
+const kimenet = document.querySelector('#kimenet');
+
+const valtozas = () => {
+    let bgc = document.querySelector('#bgc').value;
+    kimenet.style.backgroundColor = bgc;
+    let bgi = document.querySelector('#bgi').value;
+    kimenet.style.backgroundImage = `url(${bgi})`;
+    let bgr = document.querySelector('#bgr').value;
+    kimenet.style.backgroundRepeat = bgr;
+    let bgo = document.querySelector('#bgo').value;
+    kimenet.style.backgroundOrigin = bgo;
+};
